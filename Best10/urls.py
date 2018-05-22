2640311dc7e2a849e319ff92a80e90bde5f5f3e9
Cj <cj@Cjs-MacBook-Pro.local>
2018-05-22 08:17:20 -0700

    modify the design

--- Best10/urls.py
+++ Best10/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from items.views import item_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',item_list, name="item-list")
+    url(r'^$',item_list, name="item-list"),
+    url(r'^', include('landing_page.urls', namespace='landing_page'))
+
 ]
