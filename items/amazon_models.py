f628596c37d648ff629028a447547965b2956217
Cj <cj@Cjs-MacBook-Pro.local>
2018-06-13 08:13:53 -0700

    add 29 seconds timeout

--- items/amazon_models.py
+++ items/amazon_models.py
@@ -59,7 +59,7 @@ class Item(object):
                                  headers=headers,
                                  proxies=proxies,
                                  verify=False,
-                                 # timeout=29
+                                 timeout=29
                                  )
 
 
