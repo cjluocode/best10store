b8d8a40d7659f1ce4cdf0686f1f39a9cc6ff5ff7
Cj <cj@Cjs-MacBook-Pro.local>
2018-06-12 21:16:27 -0700

    add timeout to requests url

--- items/amazon_models.py
+++ items/amazon_models.py
@@ -59,6 +59,7 @@ class Item(object):
                                  headers=headers,
                                  proxies=proxies,
                                  verify=False,
+                                 timeout=29
                                  )
 
 
