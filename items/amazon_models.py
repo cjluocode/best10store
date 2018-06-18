64304abd56d57376d53f293958a45d3aa53c387d
Maksim <maksimuc24web@gmail.com>
2018-06-18 14:06:31 +0300

    proxies updates

--- items/amazon_models.py
+++ items/amazon_models.py
@@ -38,83 +38,65 @@ class Item(object):
                 'User-Agent': user_agent,
             }
 
-            #Set url
+            # Set url
             pre_url = 'https://www.amazon.com/s?url=search-alias%3Daps'
             keyword_url = '&field-keywords=%s' % q_word
             url = pre_url + keyword_url + '&page={0}'.format(page)
 
-
-            # Set Proxy
-            proxy_host = "proxy.crawlera.com"
-            proxy_port = "8010"
-            proxy_auth = "5b115385a7f3490bbbb35fa44d8b9bf9:"
-            proxies = {"https": "https://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port),
-                       "http": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port)}
+            r = self.load_page(url=url, headers = headers, is_proxy= True)
 
 
-            try:
-                print("requesting getting url")
-                r = requests.get(url,
-                                 headers=headers,
-                                 proxies=proxies,
-                                 verify=False,
-                                 timeout=29
-                                 )
+            print("requesting getting url")
+            print("status_code: " + str(r.status_code))
 
+            if int(r.status_code) == 200:
+                try:
+                    parser = html.fromstring(r.content)
+                    all_item_container = parser.xpath(XPATH_ITEM_CONTAINER)
 
-                print("status_code: " + str(r.status_code))
+                    for item in all_item_container:
 
-                if int(r.status_code) == 200:
-                    try:
-                        parser = html.fromstring(r.content)
-                        all_item_container = parser.xpath(XPATH_ITEM_CONTAINER)
+                        # Get the title
+                        raw_title = item.xpath(XPATH_TITLE)
+                        if len(raw_title) > 0:
+                            title = raw_title[0]
+                            print(title)
 
-                        for item in all_item_container:
+                        # Get the Link
+                        raw_link = item.xpath(XPATH_LINK)
+                        if len(raw_link) > 0:
+                            link = raw_link[0]
 
-                            # Get the title
-                            raw_title = item.xpath(XPATH_TITLE)
-                            if len(raw_title) > 0:
-                                title = raw_title[0]
-                                print(title)
+                        # Get image
+                        raw_image = item.xpath(XPATH_IMAGE)
+                        if len(raw_image) >= 1:
+                            image = raw_image[-1]
 
-                            # Get the Link
-                            raw_link = item.xpath(XPATH_LINK)
-                            if len(raw_link) > 0:
-                                link = raw_link[0]
+                        # Get rating counts
+                        raw_rating_counts = item.xpath(XPATH_RATING_COUNT)
+                        if len(raw_rating_counts) >= 1:
+                            raw_rating_counts = raw_rating_counts[-1].text
+                            rating_counts = int(raw_rating_counts.replace(',', ''))
 
-                            # Get image
-                            raw_image = item.xpath(XPATH_IMAGE)
-                            if len(raw_image) >= 1:
-                                image = raw_image[-1]
+                        # Get the ratings
+                        raw_rating = item.xpath(XPATH_RATING)
+                        if len(raw_rating) >= 1:
+                            rating = float(raw_rating[-1].split("out")[0])
 
-                            # Get rating counts
-                            raw_rating_counts = item.xpath(XPATH_RATING_COUNT)
-                            if len(raw_rating_counts) >= 1:
-                                raw_rating_counts = raw_rating_counts[-1].text
-                                rating_counts = int(raw_rating_counts.replace(',', ''))
+                        # Create new item then append to
+                        new_item = Item()
+                        new_item.title = title
+                        new_item.link = link
+                        new_item.image = image
+                        new_item.rating_count = rating_counts
+                        new_item.rating = rating
 
-                            # Get the ratings
-                            raw_rating = item.xpath(XPATH_RATING)
-                            if len(raw_rating) >= 1:
-                                rating = float(raw_rating[-1].split("out")[0])
+                        item_list.append(new_item)
 
-                            # Create new item then append to
-                            new_item = Item()
-                            new_item.title = title
-                            new_item.link = link
-                            new_item.image = image
-                            new_item.rating_count = rating_counts
-                            new_item.rating = rating
+                except Exception as e:
+                    print(e)
 
-                            item_list.append(new_item)
-
-                    except Exception as e:
-                        print(e)
-
-                print("--- %s seconds ---" % (time.time() - start_time))
-
-            except:
-                pass
+            print("--- %s seconds ---" % (time.time() - start_time))
 
         sorted_item_list = self.sort_item_list(item_list)
 
@@ -135,3 +117,62 @@ class Item(object):
         rating_sort = sorted(rating_count_sort[:10], key=lambda x: x.rating, reverse=True)
 
         return rating_sort
+
+        # TODO: load page into bs4
+
+    def load_page(self, url, headers, is_proxy=False, max_try_num=20):
+        print("[+++][PROXY] Now url is : {}".format(url))
+        soup = ""
+        try_num = 1
+        while try_num <= max_try_num:
+            print("[?] Proxy try {}".format(try_num))
+            try:
+                if is_proxy:
+                    proxies = {
+                        'https': self.getProxy(),
+                    }
+                    response = requests.get(url=url, headers=headers, proxies=proxies, timeout=5)
+                else:
+                    response = requests.get(url=url, headers=headers, timeout=5)
+                try:
+                    if response.status_code == 200:
+                        print("[+++] Proxy success")
+                        soup = response
+                        break
+                    elif response.status_code == 404:
+                        soup = response
+                        try_num += 1
+                        continue
+                    elif response.status_code == 400:
+                        print("[-] Response code is {}".format(response.status_code))
+                        soup = response
+                        break
+                    else:
+                        print("[-] Response code is {}".format(response.status_code))
+                except Exception as E:
+                    try_num += 1
+                    soup = response
+                    continue
+
+            except Exception as E:
+                try_num += 1
+
+        return soup
+
+        # TODO: get proxy from list
+
+    def getProxy(self):
+        # Put here your proxies like <ip:port>
+        proxies = [
+            #"108.59.14.208:13081",
+            #"37.48.118.90:13081"
+        ]
+
+        if len(proxies) == 1:
+            number = 0
+        else:
+            number = random.randrange(0, len(proxies) - 1, 1)
+        proxy = proxies[number]
+
+        print("::Now proxy is : {}".format(proxy))
+        return proxy
