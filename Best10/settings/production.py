b3bfc3867f0975f1289d3af0869591f399890578
cjluocode <cj.best10store@gmail.com>
2018-06-21 23:20:48 -0700

    modify the homepage wording

--- Best10/settings/production.py
+++ Best10/settings/production.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'gd9dk-yvcsdhkjhalkas8tx#d6z_v5ynm%y9pgzakjsdkhaljjjq@%5@+if5)ya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.best10.store', 'best-10.herokuapp.com','best10.store']
 
