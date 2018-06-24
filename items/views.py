a2e3cc3662c35560da49a0b2710c03457fca8bae
cjluocode <cj.best10store@gmail.com>
2018-06-24 10:12:36 -0700

    add ramdom number

--- items/views.py
+++ items/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from random import randint
 
 # Create your views here.
 from django.shortcuts import render
@@ -6,13 +7,16 @@ from .amazon_models import Item
 
 # Home Page
 def item_list(request):
+    total_items_number = randint(239, 278)
+
     if request.method == "POST":
         q_word = request.POST['query_word']
         if q_word:
             item = Item()
             search_result = item.get_items(q_word=q_word)
             context = {
-             "item_list": search_result
+             "item_list": search_result,
+             "total_items_number" : total_items_number,
             }
             return render(request,'items/item_list.html', context)
         else:
